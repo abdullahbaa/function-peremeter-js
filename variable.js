fdf8de2deb5136b898ee7c00906d53ad25b31fe6
abdullahraihan03 <aaziz191231@bscse.uiu.ac.bd>
2024-11-23 20:33:27 +0600

    added recap section

---
+++ variable.js
@@ -0,0 +1,27 @@
+/**
+ * .........when you declare a variable.......
+ * 
+ * Primitive
+ * 1- Null
+ * 2- Undefined
+ * 3-Number
+ * 4-Boolean
+ * 5-String
+ * 
+ * Non-Primitive
+ * 1-Array
+ * 2-Object
+ */
+
+
+/**
+ * ....Conditional......
+ * 1-   >
+ * 2-   <
+ * 3-   >=
+ * 4-   <=
+ * 5-   ===
+ * 6-   !==
+ * 7-   &&
+ * 8-   ||
+ */
